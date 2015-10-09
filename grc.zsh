cb826c562c3baa678bdbdc8ab352e1ef2bb9dce1
Andrew Wong <wongadev@gmail.com>
2015-10-10 05:05:29 +1100

    grc.zsh: fix indentation

--- grc.zsh
+++ grc.zsh
@@ -30,9 +30,9 @@ if [[ "$TERM" != dumb ]] && (( $+commands[grc] )) ; then
 
   # Set alias for available commands.
   for cmd in $cmds ; do
-  if (( $+commands[$cmd] )) ; then
-    alias $cmd="grc --colour=auto $cmd"
-  fi
+    if (( $+commands[$cmd] )) ; then
+      alias $cmd="grc --colour=auto $cmd"
+    fi
   done
 
   # Clean up variables
