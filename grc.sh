2086ee753aaab7e1f78f557d86fc160fcd0302fb
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2022-04-22 20:44:10 -0400

    refactor bash alias generation
    * grc.sh (set_grc_aliases): Use an array to generate aliases.

--- grc.sh
+++ grc.sh
@@ -9,50 +9,58 @@ set_grc_aliases () {
     [ -z "$GRC_ALIASES" ] && [ -f /etc/default/grc ] && . /etc/default/grc
     [ "$GRC_ALIASES" = "true" ] && tty -s && [ -n "$TERM" ] && [ "$TERM" != dumb ] && [ -n "$grc" ] ||
             return 0
+
     alias colourify="$grc -es"
-    alias blkid='colourify blkid'
+    local cmds=(
+        as
+        blkid
+        df
+        diff
+        dig
+        docker
+        docker-compose
+        docker-machine
+        du
+        # env
+        fdisk
+        findmnt
+        free
+        g++
+        gas
+        gcc
+        getsebool
+        gpg
+        head
+        id
+        ifconfig
+        ip
+        iptables
+        journalctl
+        kubectl
+        ld
+        # ls
+        lsblk
+        lsof
+        lspci
+        make
+        mount
+        mtr
+        netstat
+        ping
+        ps
+        semanage
+        sockstat
+        ss
+        tail
+        traceroute
+        traceroute6
+    )
+
+    for name in ${cmds[@]}; do
+        alias $name="colourify $name"
+    done
+
     alias configure='colourify ./configure'
-    alias df='colourify df'
-    alias diff='colourify diff'
-    alias docker='colourify docker'
-    alias docker-compose='colourify docker-compose'
-    alias docker-machine='colourify docker-machine'
-    alias du='colourify du'
-#    alias env='colourify env'
-    alias free='colourify free'
-    alias fdisk='colourify fdisk'
-    alias findmnt='colourify findmnt'
-    alias make='colourify make'
-    alias gcc='colourify gcc'
-    alias g++='colourify g++'
-    alias gpg='colourify gpg'
-    alias id='colourify id'
-    alias ip='colourify ip'
-    alias iptables='colourify iptables'
-    alias as='colourify as'
-    alias gas='colourify gas'
-    alias journalctl='colourify journalctl'
-    alias kubectl='colourify kubectl'
-    alias ld='colourify ld'
-    #alias ls='colourify ls'
-    alias lsof='colourify lsof'
-    alias lsblk='colourify lsblk'
-    alias lspci='colourify lspci'
-    alias netstat='colourify netstat'
-    alias ping='colourify ping'
-    alias ss='colourify ss'
-    alias traceroute='colourify traceroute'
-    alias traceroute6='colourify traceroute6'
-    alias head='colourify head'
-    alias tail='colourify tail'
-    alias dig='colourify dig'
-    alias mount='colourify mount'
-    alias ps='colourify ps'
-    alias mtr='colourify mtr'
-    alias semanage='colourify semanage'
-    alias getsebool='colourify getsebool'
-    alias ifconfig='colourify ifconfig'
-    alias sockstat='colourify sockstat'
 }
 
 set_grc_aliases
